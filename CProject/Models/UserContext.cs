f7961f3314147ab98ed09da54bd1537dad528219
Alexey <nice.isakin@mail.ru>
2022-01-27 22:23:05 +0300

    Были подготовлены модели для товаров и компаний (NR)

--- CProject/Models/UserContext.cs
+++ CProject/Models/UserContext.cs
@@ -5,6 +5,9 @@ namespace CProject.Models
     public class UserContext : DbContext
     {
         public DbSet<User> Users { get; set; }
+        public DbSet<Product> Product { get; set; }
+        public DbSet<Company> Company { get; set; }
+
         public UserContext(DbContextOptions<UserContext> options) : base(options)
         {
             Database.EnsureCreated();
